150e0f16e81f7217b6c34f7f1bfb83e27c99e8fa
dtkachov <denis_tkachov@yahoo.com>
2015-07-11 19:12:22 +0300

    version working with drawing route using sibgle points

--- RouteHotel/scripts/RouteDisplay.js
+++ RouteHotel/scripts/RouteDisplay.js
@@ -57,23 +57,97 @@ function processSteps(steps) {
     for (var i = 0; i < steps.length; ++i) {
         var step = steps[i];
 
-        processStep(step);
+        processStep(step, i);
     }
 }
 
-function processStep(step) {
+function processStep(step, index) {
+    if (null == step) return;
 
+    const STEP_MARKER_SCALE = 3;
+    const STEP_MARKER_OPACITY = 0.3;
+    const START_STEP_COLOR = "blue";
+    const END_STEP_COLOR = "gren";
+
+    var startPosition = convertToLatLng(step.StartLocation);
+    var startMarker = new google.maps.Marker({
+        position: startPosition,
+        icon: {
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: STEP_MARKER_SCALE,
+            strokeColor: START_STEP_COLOR,
+        },
+        draggable: true,
+        opacity: STEP_MARKER_OPACITY,
+        map: map
+    });
+
+    google.maps.event.addListener(startMarker, 'click', function () {
+        var html =
+             "Start point for step #" + index + "<br>" +
+             startPosition.lat().toFixed(4) + " latitude<br/>" +
+             startPosition.lng().toFixed(4) + " longitude";
+        var infowindow = new google.maps.InfoWindow({
+            content: html
+        });
+        infowindow.open(map, startMarker);
+    });
+
+    // start and end point coinsides - no reason to add end point to the map
+    /*
+    var endPosition = convertToLatLng(step.EndLocation);
+    var endMarker = new google.maps.Marker({
+        position: endPosition,
+        icon: {
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: STEP_MARKER_SCALE,
+            strokeColor: END_STEP_COLOR,
+        },
+        draggable: true,
+        opacity: STEP_MARKER_OPACITY,
+        map: map
+    });
+
+    google.maps.event.addListener(endMarker, 'click', function () {
+        var html =
+             "End point for step #" + index + "<br>" +
+             endPosition.lat().toFixed(4) + " latitude<br/>" +
+             endPosition.lng().toFixed(4) + " longitude";
+        var infowindow = new google.maps.InfoWindow({
+            content: html
+        });
+        infowindow.open(map, endMarker);
+    });
+    */
+
+    processPoints(step.Points);
 }
 
-/*
-var marker = new google.maps.Marker({
-    position: map.getCenter(),
-    icon: {
-        path: google.maps.SymbolPath.CIRCLE,
-        scale: 10
-    },
-    draggable: true,
-    map: map
-});
+function processPoints(points) {
+    if (null == points) return;
 
-*/
+    for (var i = 0; i < points.length; ++i) {
+        var point = points[i];
+
+        processPoint(point);
+    }
+}
+
+function processPoint(point) {
+    const POINT_MARKER_SCALE = 2;
+    const POINT_MARKER_OPACITY = 0.2;
+    const POINT_COLOR = "blue";
+
+    var position = convertToLatLng(point);
+    var startMarker = new google.maps.Marker({
+        position: position,
+        icon: {
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: POINT_MARKER_SCALE,
+            strokeColor: POINT_COLOR,
+        },
+        draggable: true,
+        opacity: POINT_MARKER_OPACITY,
+        map: map
+    });
+}
