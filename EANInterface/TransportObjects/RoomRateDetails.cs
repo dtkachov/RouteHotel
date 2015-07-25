8224bfbe275946c8be13f276e44758b61d757fd9
dtkachov <denis_tkachov@yahoo.com>
2015-07-25 17:04:39 +0300

    trying to fix manual serializer

---
+++ EANInterface/TransportObjects/RoomRateDetails.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EANInterface.TransportObjects
+{
+    /// <summary>
+    /// Details for the room rate
+    /// </summary>
+    public class RoomRateDetails : HotelInterface.TransportObjects.RoomRateDetails
+    {
+        /// <summary>
+        /// Response dynamic data
+        /// </summary>
+        private dynamic ResponseData;
+
+        public RoomRateDetails(dynamic responseData)
+        {
+            this.ResponseData = responseData;
+            ParseData();
+        }
+
+        /// <summary>
+        /// Parses Response data
+        /// </summary>
+        public void ParseData()
+        {
+            this.MaxRoomOccupancy = (int)ResponseData.maxRoomOccupancy;
+            this.QuotedRoomOccupancy = (int)ResponseData.quotedRoomOccupancy;
+            this.MinGuestAge = (int)ResponseData.minGuestAge;
+            this.RoomDescription = ResponseData.roomDescription;
+            this.PropertyAvailable = ResponseData.propertyAvailable;
+            this.PropertyRestricted = ResponseData.propertyRestricted;
+
+            ParseRate();
+        }
+
+        private void ParseRate()
+        {
+            // parse rate 
+            var rateInfos = ResponseData.RateInfos;
+            var rateInfo = (rateInfos.IsArray) ? rateInfos[0] : rateInfos.RateInfo;
+
+            var chargeableRateInfo = rateInfo.ChargeableRateInfo;
+
+            string itemName = string.Empty;
+            foreach (var item in chargeableRateInfo)
+            {
+                    itemName = item.Key.ToString() + " - " + item.Value.ToString();  
+            }
+
+            this.Price = chargeableRateInfo.total;
+            this.Currency = chargeableRateInfo.currencyCode;
+
+        }
+    }
+}
---
+++ HotelInterface/TransportObjects/RoomRateDetails.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelInterface.TransportObjects
+{
+    public class RoomRateDetails
+    {
+        #region dama sample
+        /*
+                            "maxRoomOccupancy": 4,
+                            "quotedRoomOccupancy": 2,
+                            "minGuestAge": 0,
+                            "roomDescription": "Номер «Делюкс», 2 двоспальних ліжка, з видом на місто",
+                            "propertyAvailable": true,
+                            "propertyRestricted": false,
+                            
+        price
+            currency
+         */
+        #endregion
+
+        public int MaxRoomOccupancy { get; set; }
+        public int QuotedRoomOccupancy { get; set; }
+        public int MinGuestAge { get; set; }
+        public string RoomDescription { get; set; }
+        public bool PropertyAvailable { get; set; }
+        public bool PropertyRestricted { get; set; }
+
+        public double Price { get; set; }
+        public string Currency { get; set; }
+    }
+}
