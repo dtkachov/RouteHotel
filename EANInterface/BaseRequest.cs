5962aeafcc6b16b3e0e4b5eb7928df57744e3b6d
dtkachov <denis_tkachov@yahoo.com>
2017-05-29 16:28:55 +0300

    Add files via upload

---
+++ EANInterface/BaseRequest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EANInterface
+{
+    /// <summary>
+    /// represents base server request
+    /// </summary>
+    public abstract class BaseRequest
+    {
+        protected static readonly log4net.ILog Log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        /// <summary>
+        /// URL to make request to
+        /// </summary>
+        public Uri URL
+        {
+            get { return _URL; }
+        }
+        private Uri _URL;
+
+        /// <summary>
+        /// .ctor
+        /// </summary>
+        /// <param name="URL">URL parameter to make request to</param>
+        protected void Initialize(Uri URL)
+        {
+            this._URL = URL;
+        }
+
+        /// <summary>
+        /// Performs web request and parses data
+        /// </summary>
+        public virtual void MakeRequest()
+        {
+            MakeRequestInt();
+            CheckStatus();
+        }
+
+        /// <summary>
+        /// Performs request to server
+        /// </summary>
+        private void MakeRequestInt()
+        {
+            Log.DebugFormat("Making request with URL '{0}'", URL);
+
+            string responseStr = WebUtils.Utils.DownloadStringFromWeb(URL);
+
+            Log.DebugFormat("Received responce '{0}'", responseStr);
+            
+            InitResponce(responseStr);
+        }
+
+        /// <summary>
+        /// Initializs response object
+        /// </summary>
+        /// <param name="responseStr">Downloaded responce</param>
+        protected abstract void InitResponce(string responseStr);
+
+        /// <summary>
+        /// Checks Response status
+        /// </summary>
+        protected abstract void CheckStatus();
+
+    }
+}
---
+++ EANInterface/RequestParametersKeys.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EANInterface
+{
+    /// <summary>
+    /// Keys for request parameters
+    /// </summary>
+    public class RequestParametersKeys
+    {
+        public const string API_KEY = "apiKey";
+        public const string CID = "cid";
+        public const string MINOR_REV = "minorRev";
+        public const string LOCALE = "locale";
+        public const string CURRENCY_CODE = "currencyCode";
+        public const string LATITUDE = "latitude";
+        public const string LONGITUDE = "longitude";
+        public const string ARRIVAL_DATE = "arrivalDate";
+        public const string DEPARTURE_DATE = "departureDate";
+        public const string ROOM = "room";
+        public const string SEARCH_RADIUS = "searchRadius";
+        public const string SEARCH_RADIUS_UNIT = "searchRadiusUnit";
+        
+        public const string DATE_TIME_FORMAT = "MM/dd/yyyy";
+
+        public static CultureInfo Culture { get { return CultureInfo.InvariantCulture; } }
+    }
+}
